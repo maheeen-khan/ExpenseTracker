b14515a0db707e7238cc72180385ec5eb3f8a71d
maheeen-khan <maheeenkhan4@gmail.com>
2024-12-05 12:55:03 -0800

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
 
   title = 'exptracker';
 
-  expArray = [{expName: 'Grocery', expAmount:13000}];
+  expArray = [{expName: 'Grocery', expAmount:13000, isEditable: false}];
 
   onSubmit(form : NgForm){
 
@@ -21,7 +21,8 @@ export class AppComponent {
 
     this.expArray.push({
       expName: form.controls['exp-name'].value,
-      expAmount: form.controls['exp-amount'].value
+      expAmount: form.controls['exp-amount'].value,
+      isEditable:false,
     })
     form.reset()
   }
@@ -35,7 +36,71 @@ export class AppComponent {
       title: "Deleted!",
       text: "Your expense record has been deleted.",
       showConfirmButton: false,
-      timer: 1500
+      timer: 2500
     });
   }
+ isEditable: boolean = false;
+ onEdit(index: number) {
+  const currentExpense = this.expArray[index];
+
+  // Toggle the isEditable state
+  // currentExpense.isEditable = !currentExpense.isEditable;
+
+  // if (!currentExpense.isEditable) {
+  //   // Save logic: Perform any additional actions needed after saving
+  //   console.log('Saved:', currentExpense.expName);
+  // } else {
+  //   console.log('Edit mode enabled for:', currentExpense.expName);
+  // }
+  var temp = prompt("Edit",`${currentExpense.expName}`);
+  var oldName = currentExpense.expName;
+
+  currentExpense.expName = temp || ''; 
+
+  if(currentExpense.expName === ""){
+    // alert("Expense name should not be remain empty!!! \n Do you mean to Delete this record ?")
+
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success mx-2",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "Expense name cannot be left empty. Are you sure you want to delete this record instead?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.onDelete(index) //delete this record
+
+        swalWithBootstrapButtons.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        currentExpense.expName = oldName;
+
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your record did not Deleted :)",
+          icon: "error"
+        });
+      }
+    });
+
+  }
+}
+
+
 }
